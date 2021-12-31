3284b0e22cab84a0c731418207dee3b3ca482175
Juan G <86728893+JuanOnly@users.noreply.github.com>
2021-12-31 09:37:06 -0500

    working chat with timestamp

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
@@ -8,13 +10,18 @@ export default function Home() {
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="lg:px-14 h-screen overflow-y-scroll bg-gradient-to-b from-black to-blue-900 overflow-hidden">
       <Head>
         <title>MetaVerse Chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to the app</h1>
-      <button onClick={logout}>Logout</button>
+
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+        <Messages />
+      </div>
+
+      <button onClick={logout}></button>
     </div>
   );
 }
