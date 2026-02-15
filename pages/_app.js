a24a819ac2963fe873fd673e2750405aa8d6c894
JuanOnly <juan@example.com>
2026-02-15 17:46:07 -0500

    Add appId to MoralisProvider

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID || "metaverse-chat"}
       apiKey={process.env.NEXT_PUBLIC_MORALIS_API_KEY}
     >
       <Component {...pageProps} />
