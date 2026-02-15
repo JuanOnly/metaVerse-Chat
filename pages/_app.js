b58356716cb0f66cffac7dc70fb65be0dfc36d97
JuanOnly <juan@example.com>
2026-02-15 17:53:59 -0500

    Add serverUrl placeholder

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
       appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID || "metaverse-chat"}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL || "https://metaverse-chat.moralis.io"}
       apiKey={process.env.NEXT_PUBLIC_MORALIS_API_KEY}
     >
       <Component {...pageProps} />
